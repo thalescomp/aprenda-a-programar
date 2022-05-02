143d64956a3eb4da46993bd95001930c091669cc
Thales Carvalho <thales.comp@gmail.com>
2022-05-02 18:50:57 -0300

    Adicionando todos os códigos exemplos

---
+++ aula-extra/tarefa3-2.js
@@ -0,0 +1,14 @@
+/*
+Um cliente que vende produtos internacionais e nacionais conseguiu extrair alguns dados de seu programa financeiro. O problema é que esse programa retorna os dados na forma de mapas e traz apenas o valor do produto e se ele é internacional ou nacional, mas não calcula os impostos.
+
+Dada essa situação, ele contratou você para criar um programa que, ao receber o mapa de um produto, verifica se ele é nacional ou internacional e aplica 20% de impostos caso seja de outro país e 12% caso seja brasileiro. Uma vez aplicado o imposto, o programa imprime na tela o valor total do produto.
+
+Base de dados que o cliente passou para você:
+
+let produtoA = {"nome": "camiseta", "valor": 100.00, "internacional": true};
+let produtoB = {"nome": "perfume", "valor": 200.00, "internacional": true};
+let produtoC = {"nome": "sandália", "valor": 120.00, "internacional": false};
+
+Copie essa base para o seu código e escreva um programa que resolve o problema do cliente e exibe as informações dos produtos e o valor aplicado os impostos devidos. Você deve testar o seu código com os 3 produtos.
+*/
+
---
+++ aula-extra/tarefa4-2.js
@@ -0,0 +1,17 @@
+/*
+A rede de boates Curtição a Mil necessita de uma funcionalidade no sistema de entrada que faz uma leitura digital da carteira de identidade das pessoas e determina se cada uma delas é ou não é maior de idade. Para a parte de reconhecimento de imagem, eles contrataram pessoas especialistas em processamento de imagens que coletassem a informação de idade direto da carteira de identidade das pessoas. O próximo passo é justamente determinar se a pessoa é maior de idade ou não. Para isso a rede Curtição a Mil contratou você para resolver esse problema.
+
+As pessoas especialistas em processamento de imagens já deixaram um template de código pra você. Com isso o seu papel é apenas preencher as informações que estão faltando para que o código funcione corretamente. Portanto, copie e cole o código abaixo em um arquivo e depois implemente a solução pedida nos comentários do código:
+*/
+
+function legalAgeOrNot (age) {
+    // escreva aqui a solução de código que verifica se a idade da pessoa é maior ou não que 18 anos.
+    // Se for maior ou igual, você deve exibir na tela "A idade é maior ou igual a 18 anos. Pode entrar na boate.".
+    // Se for menor, você deve exibir na tela "A idade é menor que 18 anos, entrada não permitida!"
+}
+
+let age1 = 13
+let age2 = 18
+let age3 = 20
+
+// chame a função legalAgeOrNot que você construiu logo abaixo deste comentário para cada uma das idades em age1, age2, age3.
