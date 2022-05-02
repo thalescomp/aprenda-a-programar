bc5a19760a34071dae722e49b93cfb9e5677b6a4
Thales Carvalho <thales.comp@gmail.com>
2022-05-02 20:29:22 -0300

    Criar função de soma

--- aula-extra/helloworld.js
+++ aula-extra/helloworld.js
@@ -1,1 +1,9 @@
-console.log("hello world!")
+let number1 = 1, number2 = 2
+
+function sum2Numbers(param1, param2) {
+    let soma = param1 + param2
+    return soma
+}
+
+let resultadoDaSoma = sum2Numbers(number1, number2)
+console.log(sum2Numbers(number1, number2))
